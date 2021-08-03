926e2a7bb4db402eb1404fc65fb8e2614750560d
Jessica Ayran <jexica@jessicas-mbp-2.myfiosgateway.com>
2021-08-03 12:45:47 -0400

    first pass at MVP

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-const DOMAIN = 'http://api.giphy.com/v1/stickers/search';
+const DOMAIN = 'http://api.giphy.com/v1/stickers/search'
 const API_KEY = 'evsVx9tZ3CipCmZgREMZqaCdKPGn4beP'
-const NAME = document.querySelector(`#name`)[0].value;
-const BASE_URL = `${DOMAIN}?api_key=${API_KEY}&q=${NAME}`;
+const NAME = document.querySelector(`#name`);
+const BASE_URL = `${DOMAIN}?api_key=${API_KEY}&q=`;
 
+const button = document.querySelector("#search")
+
+async function getGif() {
+  const response = await axios.get(`${BASE_URL}${NAME.value}`)
+  showGifData(response.data.data)
+}
+
+button.addEventListener('click', getGif)
+
+
+function showGifData(gifs) {
+  gifs.forEach((gif, index) => {
+  //append gif to container
+    // let gifElements = `<img src="${gif.images.downsized_medium.url}">`
+    let gifElements = `
+    <div id="gif-${index}">
+      <img src="${gif.images.downsized_medium.url}">
+    </div>`
+    document.querySelector('#gif-container').insertAdjacentHTML('beforeend', gifElements) 
+  });
+  
+  
+}
