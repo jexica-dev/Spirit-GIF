ae0fef946d5d5fc8d5a5c55d68d15878405789a1
Jessica Ayran <jexica@jessicas-mbp-2.myfiosgateway.com>
2021-08-06 11:32:14 -0400

    update

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const DOMAIN = "http://api.giphy.com/v1/stickers/search";
+const DOMAIN = "https://api.giphy.com/v1/stickers/search";
 const API_KEY = "evsVx9tZ3CipCmZgREMZqaCdKPGn4beP";
 const NAME = document.querySelector(`#name`);
 const BASE_URL = `${DOMAIN}?api_key=${API_KEY}&q=`;
