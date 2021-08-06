75c43297d6a38e4c3c9f5cdcc6f1526c3ebbeda5
Jessica Ayran <jexica@jessicas-mbp-2.myfiosgateway.com>
2021-08-06 10:52:04 -0400

    Final Changes

--- app.js
+++ app.js
@@ -1,92 +1,67 @@
-const DOMAIN = 'http://api.giphy.com/v1/stickers/search'
-const API_KEY = 'evsVx9tZ3CipCmZgREMZqaCdKPGn4beP'
+const DOMAIN = "http://api.giphy.com/v1/stickers/search";
+const API_KEY = "evsVx9tZ3CipCmZgREMZqaCdKPGn4beP";
 const NAME = document.querySelector(`#name`);
 const BASE_URL = `${DOMAIN}?api_key=${API_KEY}&q=`;
-const button = document.querySelector("#search")
-
-
+const button = document.querySelector("#search");
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
 function showAnimal(animalName) {
-
   let firstAnimal = animalName[getRandomInt(15)];
 
-  console.log(firstAnimal)
-  let animalElement =
-    `
+  console.log(firstAnimal);
+  let animalElement = `
       <img src="${firstAnimal.images.downsized_medium.url}"/>
-    `
-    document.querySelector('#animal-container').insertAdjacentHTML('beforeend', animalElement) 
-
+    `;
+  document
+    .querySelector("#animal-container")
+    .insertAdjacentHTML("beforeend", animalElement);
 }
- 
-
-
 
 function showGifData(gifs) {
   gifs.forEach((gif, index) => {
-
-  //append gif to container
-
-    if (Math.random() > .5) {
-
+    if (Math.random() > 0.5) {
       let gifElements = `
       <div id="gif-${index}" class="gif-block">
         <img class="gif" src="${gif.images.downsized_medium.url}">
       </div>
-      `
-      document.querySelector('#gif-container').insertAdjacentHTML('beforeend', gifElements)
-  
+      `;
+      document
+        .querySelector("#gif-container")
+        .insertAdjacentHTML("beforeend", gifElements);
     }
   });
-   
 }
 
-
-
-
 async function getGif() {
+  let inputName = NAME.value.toLowerCase();
 
-  let inputName = NAME.value.toLowerCase()
+  const nameFirstLetter = inputName[0];
 
-  const nameFirstLetter = inputName[0]
-  
+
   let animalName = "";
-  
-  let letters = "abcdefghijklmnopqrstuvwxyz"
-  
+
+  let letters = "abcdefghijklmnopqrstuvwxyz";
+
 
   let animals = ["Alligator", "Bear", "Cat", "Dragon", "Elephant", "Frog", "Goats", "Horse", "Iguana", "jaguars", "Kangeroo", "Lion", "cute monkey", "Narwhal", "Otter", "Penguin", "Quail", "Rabbit", "Snake", "Tiger", "Unagi", "Vulture", "Wolf", "Axolotls", "Yak", "Zebra"]
   
-  let nameFirstLetterIndex = letters.indexOf(nameFirstLetter)
-  animalName = animals[nameFirstLetterIndex]
+  let nameFirstLetterIndex = letters.indexOf(nameFirstLetter);
+  animalName = animals[nameFirstLetterIndex];
 
+  let gifContainer = document.querySelector("#gif-container");
+  gifContainer.innerHTML = "";
+  let animalContainer = document.querySelector("#animal-container");
+  animalContainer.innerHTML = "";
 
-    let gifContainer = document.querySelector('#gif-container')
-  gifContainer.innerHTML = ""
-  let animalContainer = document.querySelector('#animal-container')
-  animalContainer.innerHTML = ""
+  const response = await axios.get(`${BASE_URL}${NAME.value}`);
+  const responseAnimal = await axios.get(`${BASE_URL}${animalName}`);
 
-  const response = await axios.get(`${BASE_URL}${NAME.value}`)
-  const responseAnimal = await axios.get(`${BASE_URL}${animalName}`)
-
-  
-   console.log(responseAnimal)
-  showAnimal(responseAnimal.data.data)
-  showGifData(response.data.data)
-
-  // functionName(response.data.data) to grab the animals and append 
+  console.log(responseAnimal);
+  showAnimal(responseAnimal.data.data);
+  showGifData(response.data.data);
 }
 
-
-button.addEventListener('click', getGif)
-
-
-
-
-
- 
-
+button.addEventListener("click", getGif);
--- async.js
+++ async.js
@@ -1,69 +1,27 @@
-// Async/Await
-// Api Docs : https://developers.giphy.com/docs/api/
-
-
-// Step 1: Fetch Gif data from Giphy using .then()
-
-// function fetchData(Gif) {
-//   Get Gif 
-//   const url = ``
-// Write code here.
-//   axios.get(url)
-//     .then((res) => {
-//     console.log(res)
-//     })
-//     .catch((error) => {
-//       console.error(error)
-//     })
-// }
-
-
-// Step 2: Rewrite Step 1 using Try/Catch & Async/Await
-// Be sure to comment out your Step 1 function before writing step2.  
 async function fetchData() {
-
-  // write code here
-
   try {
-    const url = BASE_URL
-    let response = await axios.get(url)
-    console.log(response)
-    // const gifData = response.data[0]
-    // showGifData(gifData)
-    return data
+    const url = BASE_URL;
+    let response = await axios.get(url);
+    console.log(response);
+    return data;
   } catch (error) {
-    console.log(error)
-  } 
+    console.log(error);
+  }
 }
 
 fetchData();
 
-
-// Step 3: Create dynamic HTML
-
 function showGifData(data) {
-  // console.log(data.name)
-  // gif stickers
-  let gifElements = `<img src="${data.gif}">`
-    
- document.querySelector('#name').insertAdjacentHTML('beforeend', gifElements) 
+  let gifElements = `<img src="${data.gif}">`;
+
+  document.querySelector("#name").insertAdjacentHTML("beforeend", gifElements);
 }
 
-// Step 4
+const form = document.querySelector("#name-form");
 
-// Step 5
-const form = document.querySelector('#name-form')
-//console.log(form)
-//write eventHandler here
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  const textInput = document.querySelector('#name')
-  console.log(textInput.value)
-  fetchData(textInput.value)
-})
-
-
-
-
-
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const textInput = document.querySelector("#name");
+  console.log(textInput.value);
+  fetchData(textInput.value);
+});
